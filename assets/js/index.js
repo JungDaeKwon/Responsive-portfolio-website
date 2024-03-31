0341a28857905bb5b678e6803124a11f8d36033e
Jung Dae Kwon <107815603+JungDaeKwon@users.noreply.github.com>
2024-03-31 22:40:12 +0000

    before deploy

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,9 @@ modalClose.forEach((modalClose) => {
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') 
+                    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
