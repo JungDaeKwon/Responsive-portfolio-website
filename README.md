4cfeb053ea3b78193b2ae8e38093f75c58823c90
Jung Dae Kwon <107815603+JungDaeKwon@users.noreply.github.com>
2024-03-31 17:29:52 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Responsive-portfolio-website
+This project was created based on the Bedimcode's Responsive Portfolio Website Liz tutorial and video.
+
+Original source code: https://github.com/bedimcode/responsive-portfolio-website-Liz
+
+Tutorial video: https://www.youtube.com/watch?v=F2_6rFk-UC8&list=PL07efmqYWHZ_rVeQ1ws0ER9eL6cxo-d5V
+
+This project is based on the code and resources used to create Liz's personal portfolio website.
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,27 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form');
+      contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    
+    // serviceID - templateID - #form - publickKey
+    emailjs.sendForm('service_e7a2zyn', 'template_6bkr4uf', '#contact-form', 'czrjfUAfdoLUpWThk')
+        .then(() => {
+            // show sent message
+            contactMessage.textContent = 'Message sent successfully 💫'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)           
+
+            // Clear input fields
+            contactForm.reset()
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) 🚫'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
